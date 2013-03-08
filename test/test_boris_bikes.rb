487f7f0f0300bd3ab4b670056a071a019ca03bea
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-03-08 11:51:24 +0000

    boilerplate for the controlling class

---
+++ test/test_boris_bikes.rb
@@ -0,0 +1,52 @@
+require './lib/boris_bikes' # the class under test
+require 'minitest/autorun' # the minitest itself
+require 'ansi' # makes it colored
+require 'turn' # improves the default output
+
+class TestBorisBikes < MiniTest::Unit::TestCase
+
+  def setup   
+ 
+  end
+
+  def test_people_take_bikes
+
+  end
+
+  def test_van_moves_bikes_from_station_to_garage_when_there_are_broken_bikes_at_a_station
+
+  end
+
+  def test_van_moves_bikes_from_station_to_garage_when_there_are_fixed_bikes_at_the_garage
+
+  end
+
+  def test_van_moves_bikes_from_station_to_garage_when_there_are_fixed_bikes_at_the_garage
+
+  end
+
+  def test_van_moves_bikes_from_station_to_station_to_level_the_load_if_there_are_no_bikes_to_fix
+
+  end
+
+  def test_people_return_bikes
+
+  end
+
+  def test_stations_with_bikes_are_created
+
+  end
+
+  def test_people_are_created
+
+  end
+
+  def test_van_is_created
+    
+  end
+
+  def test_garage_is_created
+    
+  end
+
+end
